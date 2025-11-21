44c5acf6aadcf499ff66334ae727ba2739072cd0
swpv <swpv@email.cz>
2025-11-21 22:26:30 +0100

    edit filmů

--- src/app/add-movie-modal/add-movie-modal.component.ts
+++ src/app/add-movie-modal/add-movie-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core'; // Přidán Input a OnInit
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule, ModalController } from '@ionic/angular';
@@ -8,15 +8,30 @@ import { Movie } from '../interface/movie.interface';
   selector: 'app-add-movie-modal',
   templateUrl: './add-movie-modal.component.html',
   styleUrls: ['./add-movie-modal.component.scss'],
-  standalone: true, 
+  standalone: true,
   imports: [CommonModule, FormsModule, IonicModule]
 })
-export class AddMovieModalComponent {
+export class AddMovieModalComponent implements OnInit {
+  
+  // Pokud sem něco pošleme, uloží se to sem
+  @Input() movieData: Movie | null = null;
+
+  // S tímto objektem pracuje formulář
   movie: Partial<Movie> = {
     rating: 3
   };
 
+  isEditing = false; // Pomocná proměnná pro změnu nadpisů
+
   constructor(private modalCtrl: ModalController) {}
+
+  ngOnInit() {
+    // Pokud nám přišla data (editace), naplníme formulář
+    if (this.movieData) {
+      this.movie = { ...this.movieData }; // Uděláme kopii, ať neměníme originál hned
+      this.isEditing = true;
+    }
+  }
 
   dismiss() {
     this.modalCtrl.dismiss();
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { ModalController, ActionSheetController } from '@ionic/angular'; // <--- PŘIDAT ActionSheetController
+import { Component, OnInit } from '@angular/core';
+import { ModalController, ActionSheetController } from '@ionic/angular';
 import { AddMovieModalComponent } from '../add-movie-modal/add-movie-modal.component';
 import { Movie } from '../interface/movie.interface';
 
@@ -7,66 +7,105 @@ import { Movie } from '../interface/movie.interface';
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  standalone: false 
+  standalone: false
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
   movies: Movie[] = [];
+  currentView: string = 'collection';
 
-  // <--- PŘIDAT actionSheetCtrl DO CONSTRUCTORU
   constructor(
     private modalCtrl: ModalController,
-    private actionSheetCtrl: ActionSheetController 
+    private actionSheetCtrl: ActionSheetController
   ) {}
 
-  async openAddModal() {
+  ngOnInit() {
+    const storedData = localStorage.getItem('my_movies');
+    if (storedData) {
+      this.movies = JSON.parse(storedData);
+    }
+  }
+
+  saveMovies() {
+    localStorage.setItem('my_movies', JSON.stringify(this.movies));
+  }
+
+  getAverageRating(): number {
+    if (this.movies.length === 0) return 0;
+    const total = this.movies.reduce((sum, movie) => sum + movie.rating, 0);
+    return parseFloat((total / this.movies.length).toFixed(1));
+  }
+
+  // TATO FUNKCE JE ZMĚNĚNÁ: Nyní přijímá volitelný parametr 'movieToEdit'
+  async openAddModal(movieToEdit?: Movie) {
     const modal = await this.modalCtrl.create({
       component: AddMovieModalComponent,
-      cssClass: 'add-movie-modal-css'
+      cssClass: 'add-movie-modal-css',
+      // Zde posíláme data do modálu (pokud existují)
+      componentProps: { 
+        movieData: movieToEdit 
+      }
     });
 
     modal.onWillDismiss().then((data) => {
       if (data.role === 'confirm') {
-        const newMovie: Movie = {
-          id: Math.random().toString(),
-          ...data.data
-        };
-        this.movies.push(newMovie);
+        const resultData = data.data;
+
+        // LOGIKA: Pokud má film ID a to ID už máme v seznamu, jde o EDITACI
+        const index = this.movies.findIndex(m => m.id === resultData.id);
+
+        if (index > -1) {
+          // EDITACE: Nahradíme starý záznam novým
+          this.movies[index] = resultData;
+        } else {
+          // NOVÝ FILM: Přidáme ID a vložíme do pole
+          const newMovie: Movie = {
+            id: Math.random().toString(), // ID generujeme jen u nových
+            ...resultData
+          };
+          this.movies.push(newMovie);
+        }
+
+        this.saveMovies();
       }
     });
 
     return await modal.present();
   }
 
-  // <--- NOVÁ FUNKCE: Otevře menu s možnostmi
   async openOptions(movie: Movie) {
     const actionSheet = await this.actionSheetCtrl.create({
-      header: movie.title, // Nadpis menu bude název filmu
+      header: movie.title,
       buttons: [
+        // TLAČÍTKO PRO EDITACI
+        {
+          text: 'Edit',
+          icon: 'create',
+          handler: () => {
+            this.openAddModal(movie); // Otevřeme modál a pošleme mu tento film
+          }
+        },
         {
           text: 'Delete',
-          role: 'destructive', // Červená barva (v iOS stylu)
+          role: 'destructive',
           icon: 'trash',
           handler: () => {
-            this.deleteMovie(movie); // Zavolá smazání
+            this.deleteMovie(movie);
           }
         },
         {
           text: 'Cancel',
           icon: 'close',
           role: 'cancel',
-          handler: () => {
-            // Nic se nestane, jen se zavře
-          }
+          handler: () => {}
         }
       ]
     });
     await actionSheet.present();
   }
 
-  // <--- NOVÁ FUNKCE: Smaže film z pole
   deleteMovie(movie: Movie) {
-    // Vyfiltrujeme pole tak, že si necháme všechno KROMĚ toho filmu, co chceme smazat
     this.movies = this.movies.filter(m => m.id !== movie.id);
+    this.saveMovies();
   }
 }
